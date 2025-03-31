934fe89fd18db372679b1e86eda30ed4f023719c
Neena <neenarajendran1999@gmail.com>
2025-03-31 21:09:47 +0530

    feat: Added Login and Player Component to the Clone

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,26 @@
-import React,{ useEffect, useRef } from 'react'
+import React,{ useEffect, useRef, useState } from 'react'
 import "./TitleCards.css"
 import cards_data from '../../assets/cards/Cards_data'
 
 
 
-const TitleCards = () => {
-    
+const TitleCards = ({title, category}) => {
+  const[apiData, setApiData]=useState([]);  
 const cardsRef = useRef();
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTY0ZjRlYTdlYjQ0MzVlMDk0MWY1MzA5MzRkMGU2NiIsIm5iZiI6MTcxMDE2MDE3My41MjMsInN1YiI6IjY1ZWVmOTJkZWZlMzdjMDE4NmVlOGMwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ci33SfreLhVFjk1RFzGqvl53Ru_4zviI008s8AC_TO8'
+    }
+  };
+  
+ useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+ },[])
 
 const handleWheel = (event)=>{
     event.preventDefault();// whenever the mouse wheel is on these cards the mouse can't scroll the website horizontally
@@ -18,12 +32,12 @@ useEffect(()=>{
 },[])
   return (
     <div  className='title-cards'>
-        <h2>Popular on Netflix</h2>
+        <h2>{title? title:"Popular on Netflix"}</h2>
         <div className="card-list" ref={cardsRef}>  {/* the movie cards need to scroll without using the shift key so we are using useRef to achieve that functionality*/}
-            {cards_data.map((card,index)=>{
+            {apiData.map((card,index)=>{
                 return <div className="card" key={index} >
-                    <img src={card.image} alt="" />
-                    <p>{card.name}</p>
+                    <img src={`https://image.tmdb.org/t/p/w500/` + card.backdrop_path} alt="" />
+                    <p>{card.original_title}</p>
                 </div>
             })}
         </div>
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import "./Player.css"
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 const Player = () => {
   return (
-    <div className='player'>Player</div>
+    <div className='player'>
+      <img src={back_arrow_icon} alt="" />
+      <iframe width='90%' height='90%' src='https://www.youtube.com/embed/watch?v=6IJCfkgy_ok' frameborder="0" title='trailer' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
+    </div>
   )
 }
 
